a4da7304bd07b0208520897036431f1900249daf
jonathan <jcholeiva@gmail.com>
2015-06-30 12:54:55 -0600

    Ejemplo2
    iniciando

---
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JP
+ */
+public class Ejemplo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "hola mundo");
+        // TODO code application logic here
+    }
+    
+}
